e43c01b2ea878f4246725a2620b353a8fc89bafb
Zecheng <zc312060172@gmail.com>
2019-11-28 10:01:03 +0000

    init

---
+++ src/main/java/uk/ac/soton/ecs/team19/run1/Run.java
@@ -0,0 +1,12 @@
+package uk.ac.soton.ecs.team19.run1;
+
+/**
+ * You should develop a simple k-nearest-neighbour classifier using the “tiny image” feature.
+ * The “tiny image” feature is one of the simplest possible image representations.
+ */
+
+public class Run {
+    public static void main( String[] args ) {
+
+    }
+}
---
+++ src/main/java/uk/ac/soton/ecs/team19/run2/Run.java
@@ -0,0 +1,9 @@
+package uk.ac.soton.ecs.team19.run2;
+
+/**
+ *  You should develop a set of linear classifiers (use the LiblinearAnnotator class to automatically create 15 one-vs-all classifiers)
+ *  using a bag-of-visual-words feature based on fixed size densely-sampled pixel patches.
+ */
+public class Run {
+    public static void main( String[] args ) {}
+}
